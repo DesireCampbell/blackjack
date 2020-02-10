f7f7652117a24ff0be378a10512683eae16df84c
desir <desir@Desire-Laptop>
2020-02-10 12:00:01 -0500

    Initial design ideas - added Blackjack class, BlackjackPlayer, BlackjackDealer, BlackjackCard, Rank enum, suit enum.

---
+++ src/ca/sheridancollege/project/BlackjackDealer.java
@@ -0,0 +1,19 @@
+package ca.sheridancollege.project;
+
+public class BlackjackDealer {
+
+	private GroupOfCards deck;
+
+	public GroupOfCards getDeck() {
+		return this.deck;
+	}
+
+	/**
+	 * 
+	 * @param deck
+	 */
+	public void setDeck(GroupOfCards deck) {
+		this.deck = deck;
+	}
+
+}
